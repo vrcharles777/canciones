21f54d1013a3f74315314041b6dce50bd8f6b3cc
Carlos Alberto Vicente Ruiz <vrcharles777@gmail.com>
2024-06-30 11:18:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ function displayFiles(files, path) {
             
             // Si es un archivo HTML, cambiar el URL para que apunte a GitHub Pages
             if (fileName.endsWith('.html')) {
-                fileUrl = `https://${user}.github.io/${repo}/${path}/${fileName}`.replace(/\/\//g, '/');
+                const cleanPath = path ? `${path}/` : '';
+                fileUrl = `https://${user}.github.io/${repo}/${cleanPath}${fileName}`.replace(/\/\//g, '/');
             }
 
             li.innerHTML = `<a href="${fileUrl}" target="_blank">${fileName}</a>`;
